f1f1655b385e1994a7beb1540e8c32e6105de906
JManuel Ruiz <metalfranckie@gmail.com>
2012-07-05 22:07:45 -0500

    practica sobre formulario, dado un arreglo se crea un formulario que agregara links al menu principal

--- application/controllers/inicio.php
+++ application/controllers/inicio.php
@@ -14,13 +14,46 @@ class Inicio extends CI_Controller
 
 	function index()
 	{
-		echo "Hola";
+		$this->formulario();
 	}
 
 	function testurl()
 	{
 		$data['titulo'] = 'Mi titulo';
 		$data['dato'] = urldecode($this->uri->segment(4));
+		$this->load->view('pagina',$data);
+	}
+
+	function formulario()
+	{
+		$this->load->helper('form');
+		$data['titulo'] = 'Formulario';
+		$data['dato'] = 'dato';
+		$data['main_menu'] = array(
+				array('title'=>'Inicio','link'=>base_url(),'attrs'=>''),
+				array('title'=>'Usuarios','link'=>'Usuarios','attrs'=>'')
+			);
+		$data['formulario'] = array(
+			'title'=>'Links',
+			'action'=>'receptor',
+			'attrs'=>'class="form-horizontal"',
+			'fields'=>array(
+				array(
+					'type'=>'input',
+					'name'=>'Titulo',
+					'attrs'=>array('name'=>'titulo','id'=>'titulo','class'=>'input-large')
+				),
+				array(
+					'type'=>'input',
+					'name'=>'Enlace',
+					'attrs'=>array('name'=>'titulo','id'=>'titulo','class'=>'input-large')
+				)
+			),
+			'buttons'=>array(
+				array('type'=>'button','class'=>'btn btn-large btn-success','content'=>'Enviar')
+			)
+		);
+
 		$this->load->view('pagina',$data);
 	}
 }
--- application/views/pagina.php
+++ application/views/pagina.php
@@ -1,28 +1,68 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="es-MX">
 <head>
-	<title>Pagina curso</title>
+	<meta charset="utf-8" />
+	<title><?php echo $titulo; ?> | CodeigniterTour</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo $this->config->base_url('resources/css/bootstrap.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo $this->config->base_url('resources/css/bootstrap-responsive.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo $this->config->base_url('resources/css/formulario.css'); ?>">
+	<script type="text/javascript" src="<?php echo $this->config->base_url('resources/js/jquery-1.7.2.min.js'); ?>"></script>
+	<script type="text/javascript" src="<?php echo $this->config->base_url('resources/js/formulario.js'); ?>"></script>
 </head>
 <body>
 
-	<div class="container-fluid">
-		<div class="row-fluid">
-			<div class="page-header">
-				<h1><?php echo $titulo; ?><small>Texto</small></h1>
-			</div>	
-		</div>
-		<div class="row-fluid">
-			<div class="span4" id="pnl-left">
-				<p>Menu</p>
+	<header class="container">
+		<div class="page-header">
+			<h1><?php echo $titulo; 
+				if (isset($subtitle)) {
+					echo '<small>'.$subtitle.'</small>';
+				}?>
+			</h1>
+		</div>	
+	</header>
+
+	<div class="container">
+		<div class="row-fluid"> <?php 
+			$width_content	= 12;
+			$width_menu		= 3;
+			if (isset($main_menu) AND count($main_menu)>0): 
+				$width_content -= $width_menu; ?>
+				<div class="span<?php echo $width_menu; ?>">
+					<nav>
+						<ul class="nav nav-pills nav-stacked" id="main-menu"> 
+							<li class="nav-header">Menu</li> <?php 
+							foreach ($main_menu as $item)
+							{
+								echo '<li>'.anchor($item['link'],$item['title']).'</li>';
+							} ?>
+						</ul>
+					</nav>
+				</div> <?php 
+			endif; ?>
+			<div class="span<?php echo $width_content; ?>" id="main-content">
+				<div class="container-fluid" id="alert-messages"></div> 
+				<?php echo form_open($formulario['action'],$formulario['attrs']); ?>
+					<fieldset>
+						<legend><?php echo $formulario['title']; ?></legend> <?php 
+						foreach ($formulario['fields'] as $field): ?>
+							<div class="control-group">
+								<?php echo form_label($field['name'],$field['attrs']['name'],array('class'=>'control-label')); ?>
+								<div class="controls">
+									<?php echo form_input($field['attrs']); ?>
+								</div>
+							</div> <?php 
+						endforeach; ?>
+						<div class="form-actions"> <?php
+							foreach ($formulario['buttons'] as $button)
+							{
+								echo form_button($button);
+							} ?>
+						</div>
+					</fieldset> 
+				<?php echo form_close(); ?>
 			</div>
-			<div class="span8" id="main-content">
-				<p class="well"><?php print $dato; ?></p>
-			</div>
 		</div>
-		
 	</div>
-	
 	
 </body>
 </html>
